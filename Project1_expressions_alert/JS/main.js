25a4fcdcdc0770dbbfb2ab220715c799c6af15bb
TacTechWebdesign <118140620+TacTechWebdesign@users.noreply.github.com>
2023-01-14 08:31:55 +0800

    Basic JavaScript Projects
    Assignment

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+//window.alert method
+window.alert("Hello, world!");
+
+//document.write method
+document.write("For the last time, Hello, world!");
+
+//creating two variables
+var sent1 = "This is the beginning of the string";
+var sent2 = "and this is the end of the stinf";
+
+//displaying the variables using the document.write method
+document.write(Sent1 + Sent2);
+
+//an expression
+document.write(33 * 84);
